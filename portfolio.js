5c71187acf3ebaaff43dfd1c4589b2f7142c5d6f
AngelV02 <125764846+AngelV02@users.noreply.github.com>
2023-03-17 15:35:54 -0700

    added many function like transitions, error if no input in contact box and change of background.
    More changes will come later !

--- portfolio.js
+++ portfolio.js
@@ -6,3 +6,37 @@ function validateForm() {
     }
   }
   
+
+
+  // Get all the section elements
+const sections = document.querySelectorAll('.section');
+
+// Add a scroll event listener to the window
+window.addEventListener('scroll', () => {
+  sections.forEach(section => {
+    // Check if the section is in the viewport
+    if (isInViewPort(section)) {
+      // Add the visible class to the section
+      section.classList.add('visible');
+    }
+  });
+});
+
+// Function to check if an element is in the viewport
+function isInViewPort(element) {
+  const rect = element.getBoundingClientRect();
+  return (
+    rect.top >= 0 &&
+    rect.left >= 0 &&
+    rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+    rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+  );
+}
+
+// Get the Home section element
+const homeSection = document.querySelector('#Home');
+
+// Add the 'show' class to the Home section after the page has loaded
+window.addEventListener('load', () => {
+  homeSection.classList.add('show');
+});
